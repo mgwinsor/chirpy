671599f9f9f7715b27e2eb5041d980b447bab3c3
Michael Winsor <mgwinsor@gmail.com>
2025-04-06 11:29:19 +0800

    add: profanity filter

--- handler_validate.go
+++ handler_validate.go
@@ -3,22 +3,15 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
-
-type ErrorResponse struct {
-	Error string `json:"error"`
-}
-
-type ValidResponse struct {
-	Valid bool `json:"valid"`
-}
 
 func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Body string `json:"body"`
 	}
 	type returnVals struct {
-		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -35,5 +28,24 @@ func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	respondWithJSON(w, http.StatusOK, returnVals{Valid: true})
+	badWords := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+	cleaned := getCleanedBody(params.Body, badWords)
+
+	respondWithJSON(w, http.StatusOK, returnVals{CleanedBody: cleaned})
+}
+
+func getCleanedBody(body string, badWords map[string]struct{}) string {
+	words := strings.Split(body, " ")
+	for i, word := range words {
+		loweredWord := strings.ToLower(word)
+		if _, ok := badWords[loweredWord]; ok {
+			words[i] = "****"
+		}
+	}
+	cleaned := strings.Join(words, " ")
+	return cleaned
 }
