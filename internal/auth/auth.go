bb6bb043bf0a954b538c64a60f15ed880290595f
Michael Winsor <mgwinsor@gmail.com>
2026-09-05 21:10:54 +0800

    add: password hash and login

---
+++ internal/auth/auth.go
@@ -0,0 +1,21 @@
+package auth
+
+import (
+	"github.com/alexedwards/argon2id"
+)
+
+func HashPassword(password string) (string, error) {
+	hash, err := argon2id.CreateHash(password, argon2id.DefaultParams)
+	if err != nil {
+		return "", err
+	}
+	return hash, nil
+}
+
+func CheckPasswordHash(password, hash string) (bool, error) {
+	isMatch, _, err := argon2id.CheckHash(password, hash)
+	if err != nil {
+		return false, err
+	}
+	return isMatch, nil
+}
--- main.go
+++ main.go
@@ -52,6 +52,8 @@ func main() {
 
 	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
 
+	mux.HandleFunc("POST /api/login", apiCfg.handlerLogin)
+
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
 	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsRetrieve)
 	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerChirpsGet)
